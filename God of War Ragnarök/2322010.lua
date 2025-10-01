4e315e89aab6198f7dc017fefc4720ce703bed10
Younes <younes.imouga@gmail.com>
2025-10-01 15:26:05 +0100

    first commit

---
+++ God of War Ragnarök/2322010.lua
@@ -0,0 +1,5 @@
+-- manifest & lua provided by: https://www.piracybound.com/discord
+-- via manilua
+addappid(2322010)
+addappid(2322011, 1, "78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49")
+setManifestid(2322011, "5866573615378835835", 0)
